f185489b0d6db16f8bb70b56554dbfea1cae99dc
josebordon94 <josebordon1994@gmail.com>
2026-01-30 00:11:08 -0300

    Created startup event in main.py for rag creations

--- app/main.py
+++ app/main.py
@@ -1,21 +1,20 @@
 from fastapi import FastAPI
 from langserve import add_routes
-
 from app.rag import get_rag_chain
-
 import os
 
-print("Using LLM Provider: ", os.getenv("LLM_PROVIDER"))
 app = FastAPI(
     title="Promptior RAG Chatbot",
     version="1.0",
     description="RAG-based chatbot using LangChain"
 )
 
+@app.on_event("startup")
+def startup_event():
+    print("Using LLM Provider:", os.getenv("LLM_PROVIDER"))
 
-rag_chain_text = get_rag_chain(source="text")
-rag_chain_web = get_rag_chain(source="web")
+    rag_chain_text = get_rag_chain(source="text")
+    rag_chain_web = get_rag_chain(source="web")
 
-add_routes(app, rag_chain_text, path="/chat")
-add_routes(app, rag_chain_web, path="/chat-web")
-
+    add_routes(app, rag_chain_text, path="/chat")
+    add_routes(app, rag_chain_web, path="/chat-web")
